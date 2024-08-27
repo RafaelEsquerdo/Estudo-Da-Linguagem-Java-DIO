794bed44ce08c213c470d0b192e58ee03c1b9031
Rafael Esquerdo <rafasesquerdo@gmail.com>
2024-08-27 15:24:02 -0300

    cccc

--- Tipos-Variaveis/src/TiposVariaveis.java
+++ Tipos-Variaveis/src/TiposVariaveis.java
@@ -1,5 +1,16 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int inteiro = 1295555555;
+        long longo = 98765456789l;
+        short pequeno = 120;
+        double salarioMinimo = 2500.33;
+        inteiro = pequeno;
+        pequeno=(short) inteiro; //casting
+
+        //variaveis vs constantes
+       // O final não permite a alteração da variaveis.
+       final double Valor_De_PI = 3.14;
+       System.out.println(Valor_De_PI);
+    
     }
 }
