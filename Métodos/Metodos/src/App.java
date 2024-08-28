1efe23848665ccd45996c5776567ae233a022323
Rafael Esquerdo <rafasesquerdo@gmail.com>
2024-08-28 00:29:14 -0300

    Metodo

---
+++ Métodos/Metodos/src/App.java
@@ -0,0 +1,30 @@
+
+
+public class App {
+    public int somar;
+    public static void main(String[] args)  {
+      
+       //Métodos: Uma classe é definida por atributos e metodos. Os métodos , por sua vez, correspondem a funções ou sub-rotinas disponiveis dentro de nossas classes.
+       /*Criterios de nomeação de Métodos
+        * Esses criterios não são obrigatorios , mas é recomendado pois facilitam a vida dos programdores ao trabalharem em codigos de forma colaborativas 
+        - Deve ser nomeados como verbos
+        - Seguir o padrão  camelCase(Todas as letras minúsculas com a exceção da primeira letra da segunda palavra)
+        */
+        //Exemplos
+    }
+
+    public int somar(int n1 , int n2){
+        n1 = 2;
+        n2 =1;
+        int resultado;
+      
+       return resultado= n1+n2;
+    }
+
+
+    
+    void abrirConexao(){}
+    void  concluirProcessamento() {}
+    void findById(int id){} // não se assuste, você verá muito método em inglês em sua jornada
+    void calcularImprimir(){} // há algo de errado neste método, ele deveria ter uma única finalidade
+}
