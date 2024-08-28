f67b1a83620db74f82a3af0a1a2dbd5157890903
Rafael Esquerdo <rafasesquerdo@gmail.com>
2024-08-28 01:34:31 -0300

    Importante para o futuro javadoc

--- Palavras Reservadas/Palavras-Reservadas/src/App.java
+++ Palavras Reservadas/Palavras-Reservadas/src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       /*Palavras reservadas são identificadores de uma linguagem que já possuem uma finalidade específica, portanto não podem ser utilizados para nomear variáveis, classes, métodos ou atributos.
+
+        A linguagem Java possui 52 palavras reservadas. Todas essas palavras são classificadas em grupos e escritas com letra minúscula, sendo identificadas com uma cor especial pela maioria das IDE's. Abaixo temos a lista de palavras agrupadas por sua finalidades. */
+        /* */
+
     }
 }
